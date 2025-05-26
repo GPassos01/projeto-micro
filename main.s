a85518e7b38a668b52cbe92b951cc99c224670a7
Lucas Vieira <lucas.ferrarotto@gmail.com>
2025-05-26 17:16:15 -0300

    Print da mensagem `Entre com o comando: ` no terminal

--- main.s
+++ main.s
@@ -16,12 +16,12 @@
 .equ HEX_BASE,      0x10000020
 .equ SW_BASE,		0x10000040
 .equ KEY_BASE,		0x10000050
-.equ UART_BASE,     0x10001000
-.equ TIMER_BASE,	ox10002000
+.equ UART_BASE,     0x1000
+.equ TIMER_BASE,	0x10002000
 
 # Offset UART
 .equ UART_DADOS,    0
-.equ UART_CONTROL,	4
+.equ UART_CONTROL,	0x1004
 
 # Estados do sistema
 .equ STOPPED,		0
@@ -30,21 +30,41 @@
 
 
 _start:
-    call inicializar_sistema
+    #Print da mensagem "Entre com o comando: "
     movia		r8,	    0x10000000
 
     movia       r9,     MSG_PROMPT
+
+    PRINTF:
+
+    ldb		    r10,	 0(r9)
     
-    stwio		r9,		UART_BASE(r8)
+    beq         r10,     r0,  END
+
+    POLLING_ESCRITA:
+
+    ldwio       r11,     UART_CONTROL(r8)
+
+    andhi       r11,     r11,  0xffff
+
+    beq		    r11,     r0,   POLLING_ESCRITA
     
+    stwio		r10,	UART_BASE(r8) 
+
+    addi        r9,     r9,   1
+
+    br PRINTF
 
     #call envia_msg_uart
     
     #main_loop:
     #    call ler_comando_uart
 
+END:
+    br END
+
 .org    0x500
 MSG_PROMPT:  
-.ascii "Entre com o comando: "
+.asciz "Entre com o comando: "
 
 .end
